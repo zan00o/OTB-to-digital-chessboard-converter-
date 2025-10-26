701dca4a238989704a828f868f10b4fcf366a8bb
zan00o <zan0o@proton.me>
2025-10-25 23:27:18 -0700

    Initial GIT push with just scaffold file structure

---
+++ src/main.py
@@ -0,0 +1,16 @@
+
+"""
+File: main.py
+Authors: Ryan Lopez, Trenton Tong-Seely 
+Description: Main entry point for the chess computer vision application.
+Date: [Creation/Last modified date]
+Course: ENGR413
+"""
+
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
